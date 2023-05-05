27eb4de98be5c7be35301924f315aa3533a79cc3
KCM <egwunwokekc@gmail.com>
2023-05-05 18:30:12 +0100

    Refactored and added context

---
+++ MakeWeBet_Test/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakeWeBet_Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+
+        }
+    }
+}
---
+++ MakeWeBet_Test_Core/Services/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿using MakeWeBet_Test_Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeWeBet_Test_Core.Services.Services
+{
+    public class UserService : IUserService
+    {
+
+    }
+}
